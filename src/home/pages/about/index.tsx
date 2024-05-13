c48cba7d81815add08565198c601f626feff76f2
felipeCast <castfelipe10@gmail.com>
2024-05-13 16:46:10 -0300

    finalizando estrutura do about

--- src/home/pages/about/index.tsx
+++ src/home/pages/about/index.tsx
@@ -1,14 +1,36 @@
 import './style.css'
-
+import fotoPerfil from './alexandre.svg';
 
 export function About() {
     return (
     
-        <section className="about" id="about">
+        <section className="about">
 
-            <h1>Tela about</h1>
+        <div className="container-about">
 
-        </section>
+            <div className="about-text">
+
+                <div className="about-title">
+                    <h1>SOBRE MIM</h1>
+                    <hr />
+                </div>
+
+                <p>
+                Me chamo Alexandre, tenho 22 anos, <br />
+                natural do Rio de Janeiro. Trabalho <br />
+                na área de design há 3 anos. Estou <br />
+                pronto para te ouvir, entender e criar <br />
+                um projeto com uma abordagem única <br />
+                que atenda as suas necessidades.
+                </p>
+            
+            </div>
+
+            <div className="container-aboutImg"><img src={fotoPerfil} alt="Foto de perfil" className='image-about'/></div>
+
+        </div>
+
+    </section>
 
     )
 }
