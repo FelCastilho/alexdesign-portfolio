9ae504653ee90a190b04c50f97a567adee3afda0
felipeCast <castfelipe10@gmail.com>
2024-05-14 09:32:46 -0300

    ajustando responsividade da aba servicos

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -3,6 +3,7 @@ import './style.css'
 import { Header } from '../components/header/header';
 import { Hero } from './pages/hero/hero';
 import { About } from './pages/about';
+import { Services } from './pages/services';
 import { Projects } from './pages/projects';
 import { Contact } from './pages/contact';
 import { Footer } from '../components/footer';
@@ -13,6 +14,7 @@ function Home() {
       <Header />
       <Hero/>
       <About/>
+      <Services/>
       <Projects/>
       <Contact/>
       <Footer/>
