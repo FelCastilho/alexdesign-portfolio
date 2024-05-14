9f2c53217ea33b0d6eb1dd92d614dc3b981c0300
felipeCast <castfelipe10@gmail.com>
2024-05-14 09:29:13 -0300

    ajustes

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -3,8 +3,8 @@ import './style.css'
 import { Header } from '../components/header/header';
 import { Hero } from './pages/hero/hero';
 import { About } from './pages/about';
-import { Services } from './pages/services';
 import { Projects } from './pages/projects';
+import { Contact } from './pages/contact';
 import { Footer } from '../components/footer';
 
 function Home() {
@@ -13,8 +13,8 @@ function Home() {
       <Header />
       <Hero/>
       <About/>
-      <Services/>
       <Projects/>
+      <Contact/>
       <Footer/>
     </>
   );
