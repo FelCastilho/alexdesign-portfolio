4b2e00ff57cc9401ba13ab770386724c2d8d8d93
felipeCast <castfelipe10@gmail.com>
2024-05-16 09:09:38 -0300

    trocando as imagens do projeto clave

--- src/home/pages/clave/index.tsx
+++ src/home/pages/clave/index.tsx
@@ -32,7 +32,7 @@ export function Clave() {
 
       <div className="clave-container">
 
-        <img src={capa} alt="Capa do projeto Clave" />
+        <img src="https://lh3.googleusercontent.com/pw/AP1GczNSdi-pSv24KQ5Kj1YmOLFaciBgYBsZfULZw5R-k7YYgoyGp77-jbRJGF4ERS3QvP7rhaAQmAUUcdPx5RWTiPlONCqnlDPLhszfPwJxX8eKQ81a8XUhV-RbraaFs-ux4Bj9YRbzxxKFJiERSJfw-CY=w1314-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave" />
 
         <div className="clave-container-text">
           <p>
@@ -47,17 +47,19 @@ export function Clave() {
 
         </div>
 
-        <img src={paleta} alt="Capa do projeto Clave" />
+        <img src="https://lh3.googleusercontent.com/pw/AP1GczML1g74mZ81hwr1XgtCSBcmuynzyzQKTRp8puQDN6BmmW6ib6uXzshPhQ-VrcknBXTqfwSrUbAkspnIsAiffW_5CAS7Oan5GCpU-vBH2uv3tfbgKfNodfhbfcxsHGy4VBXJ9O8VUREb6j0CncpiMLg=w1314-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave" />
 
         <div className="clave-logo-container">
           <div className='clave-logo-apresentation1'>
-            <img src={logo1} alt="" />
-            <img src={logo2} alt="" />
+
+            <img src="https://lh3.googleusercontent.com/pw/AP1GczOt2b3Cz-b79eP-yJyhkGrBD5yqlgtH4YpY5G2IlljemJsjbRt1vMFTFprsAZSfjPvMqw781i98PubwWSnc2WaNp43FIYr5WR9IT-BXERuFKm5HreU_Bda_cOjSGd2YGzkQf1xyoenhTBN8NCLxaBw=w1314-h739-s-no-gm?authuser=3" alt="" />
+
+            <img src="https://lh3.googleusercontent.com/pw/AP1GczO3EK55xfp3k-HzCWPMdcM9RluOA_JOkcpD2sp3h5kUosEuSYUfjywGERKsQBh-zUj-ZPY1NXweH648qCsc5SGlJWVxSarW0AD1SiQ-WJ2p-C6s7fR7g7LB2IXkNJKf3JxGBEUxngv5TFbVnyv7xzA=w1314-h739-s-no-gm?authuser=3" alt="" />
           </div>
 
           <div className='clave-logo-apresentation2'>
-            <img src={logo3} alt="" />
-            <img src={logo4} alt="" />
+            <img src="https://lh3.googleusercontent.com/pw/AP1GczPhX9-ORSmuvQrEc7oIIRa6zbNCUvM65Bk6cF8UK0lbqHo9oH7_OQMxDfPq1ZhPyDj65ihjIQfvifs7VQpaUYf-tAibBp8nGvxWuNutmCHNS6kC1Lje1yBK1vjtNfBesx1Pmnry6JdNcDDevSWRfRc=w1314-h739-s-no-gm?authuser=3" alt="" />
+            <img src="https://lh3.googleusercontent.com/pw/AP1GczP1BThKwLoDI37x65GSD5SCBwPZM89d7ybjhgH2XFPihogCO7yFfIP4R0hIZIkY_SR-eqB7ZRzR_bcpIO5go2stBjGHqABF6bsHiIGp73esNgfLwCzO0WKiWBAEKtGPcUsMGA0sC1zkfdC2Wm_RN0I=w1314-h739-s-no-gm?authuser=3" alt="" />
           </div>
         </div>
 
@@ -67,14 +69,14 @@ export function Clave() {
         </div>
 
         <div className="clave-mockup-container">
-          <img src={tipografia} alt="Capa do projeto Clave" />
-          <img src={businessCard} alt="Capa do projeto Clave" />
-          <img src={businessCard2} alt="Capa do projeto Clave" />
-          <img src={adesivo} alt="Capa do projeto Clave" />
-          <img src={sacola} alt="Capa do projeto Clave" />
-          <img src={avental} alt="Capa do projeto Clave" />
-          <img src={placa} alt="Capa do projeto Clave" />
-          <img src={obrigado} alt="Capa do projeto Clave" />
+          <img src="https://lh3.googleusercontent.com/pw/AP1GczOlV1dkVZ9FAHMI8aaReGFaRH41PzdzQcIHT7SEzNBJfMtom5H0fd0e44WSsdS3mZzdLWgUa33PV9LCn-lWevXvMv_pWxkrz8DbK7xLHCjSlVs6M2RIKg0-HTjzRznHja3xFGwbvcp5V90QROfT4-A=w1314-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave" />
+          <img src="https://lh3.googleusercontent.com/pw/AP1GczMh_Rq8ohBOGOiYhLYiR-eyCR9yzXBA0nb5ykULlsfIIuR2Op3IjgtfYHRMlVZWzyDmfdZ-7gwd4Jb4RavEakSkoX3tCdsC2AamSwRMDZpOVEKrO4LG5pwD6wOriIBbBhIUhaSNd0OipwUKXJ_az4U=w1314-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave" />
+          <img src="https://lh3.googleusercontent.com/pw/AP1GczMobeP49El9rzD7wQLDuYSAzMaIY6PTDDjeXYwa0BY28DehZFciqy8qc4LRZc4v5RWcwe25bGiVkon_8wA7vv4x4Xbi7CaDLNB7mzebxo2evmdkwqd7PzupiaVkgnZphoDnuQ3MwAjO26ndZ5LggSA=w1314-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave" />
+          <img src="https://lh3.googleusercontent.com/pw/AP1GczN_xBrPdxwPlU6scBNLKyGDzTk9oisNgQ3WdUu6cXhco_Bua_zh9cCgeopSI-KUCOCUSfvXiuF75fcuARPziXTWMVlc9D_pShQuFsRsD354ZEjzQJeM41G7RwKVyjyLbNenrBqr92U8DKNnuE4Ck6o=w1314-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave" />
+          <img src="https://photos.google.com/u/3/share/AF1QipO233IA1DZ-WZbPi47OOabxjANFEjdiLMzGG0D6cW6vfqz36_TdX4F9BgXfxmtVUQ/photo/AF1QipNoOHNFx98i20SSsoM_-rkNqEz0CvP5q9tzXIhg?key=WFNXU2Fja3NqclgwWVhENE9FelhvVnU2dFV6N0ln"alt="Capa do projeto Clave" />
+          <img src="https://lh3.googleusercontent.com/pw/AP1GczPKxUSXqo_Y5b6_4s9XKDFmVIDDk7vcNBzIBYXSq2QvuN7-T5rwDJXwEv78Q2lIqFTSuTEq1QZwsIza5X-6Ksqi9cuucc4bMuPCrHnrp3tKLDGIOA3OJJKd6ymxWFa0_P9loOWUGC6iH9p2peN0tU4=w1314-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave" />
+          <img src="https://photos.google.com/u/3/share/AF1QipO233IA1DZ-WZbPi47OOabxjANFEjdiLMzGG0D6cW6vfqz36_TdX4F9BgXfxmtVUQ/photo/AF1QipO0XmNSEQGXfG54TLsMz8DXuoIYOs25u5N6lEp-?key=WFNXU2Fja3NqclgwWVhENE9FelhvVnU2dFV6N0ln" alt="Capa do projeto Clave" />
+          <img src="https://lh3.googleusercontent.com/pw/AP1GczPnnYZpF6Fy7Ye-iciAoX-rheig5wTXKoiJR4rykAn8s4UdFAVMw5oZ5gzKaW9qug5RICDjMXsjvndO6J0MUCxPa6nKD1wTz9GQrpzNz19GsrQybucHmEGCb0DbgUKDfL2RWFyVjAK0XCJ9O7RbOCI=w1440-h406-s-no-gm?authuser=3" alt="Capa do projeto Clave" />
         </div>
       </div>
 
