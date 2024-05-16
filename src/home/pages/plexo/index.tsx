317eda7fbc08c5f92d38106b79d1aae9655e9fb8
felipeCast <castfelipe10@gmail.com>
2024-05-16 11:10:59 -0300

    trocando imagens plexo

--- src/home/pages/plexo/index.tsx
+++ src/home/pages/plexo/index.tsx
@@ -2,32 +2,6 @@ import './style.css';
 
 import { Link } from 'react-router-dom';
 
-import capa from './image/1.svg';
-import paleta from './image/5.svg';
-import tipografia from './image/9.svg'
-
-import cartoes from './image/10.svg'
-import cartoes2 from './image/11.svg'
-import cartoes3 from './image/12.svg'
-import cartoes4 from './image/13.svg'
-
-import app from './image/14.svg'
-import app2 from './image/15.svg'
-import app3 from './image/16.svg'
-import app4 from './image/17.svg'
-import app5 from './image/18.svg'
-import site from './image/19.svg'
-
-import placa from './image/20.svg'
-import placa2 from './image/21.svg'
-import placa3 from './image/22.svg'
-import placa4 from './image/23.svg'
-
-import carrossel from './image/24.svg'
-import carrossel2 from './image/25.svg'
-
-import obrigado from './image/26.svg'
-
 import logo from '../../../assets/logo - header.svg'
 
 export function Plexo() {
@@ -40,7 +14,7 @@ export function Plexo() {
 
             <div className="plexo-container">
 
-                <img src={capa} alt="Capa do projeto Plexo" />
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczM5bdMDckfZem570nO4d6u9eL8AxnADNuKdiHEjydSCwtoFcae5k2YarucfUAjT0W6xMeOm0RqWcQ_ZUSflWiol5RhhowMdeN_isnB5pskr-ezPPMXB0f_fDRrSXQYgvCyOoJnajSN0Ggvst8HMh94=w1313-h739-s-no-gm?authuser=3" alt="Capa do projeto Plexo" />
 
                 <div className="plexo-container-text">
                     <p>
@@ -56,7 +30,8 @@ export function Plexo() {
                     <div className="image-3"></div>
                 </div>
 
-                <img src={paleta} alt="Capa do projeto Clave"/>
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczOrQCUgDhdrwWT3Z0zw5xz90jc8bpzJAi5iaMIZoLh7Iy9KygMvQz9f8xHI-65h-S5brUASvnlE4Q3Tpd3U21-Yu2PcM3LQhO4C3OTjaBWo-NOSfO3_XahoLpa8vmX-uIr8zPcNETBJ0kGoOSJXL-o=w1312-h739-s-no-gm?authuser=3"
+                alt="Paleta de cor projeto plexo"/>
 
                 <div className="logo-apresentation">
                     <div className="logo1"></div>
@@ -64,43 +39,43 @@ export function Plexo() {
                     <div className="logo3"></div>
                 </div>
 
-                <img src={tipografia} alt="Capa do projeto Clave"/>
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczOpehC-0mQCj8nuxFj907fKf9Wv3T1guZXN7la8Z-d7RgP7Pn_rMd_BO7mbyxzROebhosHmo7sYpbBrOh1tPw_M6j4RsFIco4_hnnPCBuuqn0SbZFSV4bZ7pInoKcVkEzFeB6est2d5G-1mY_l0ArI=w1313-h739-s-no-gm?authuser=3" alt="Tipografia"/>
 
                 <div className="mockups">
 
-                    <img src={cartoes} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczOR53t1U9inZHhoU-wqSqI8WRUc_Px8eJWxfR25ESCogiDybBxwWxe4BqKT8FwpLn2gTOzRbIahliovxHoPGzrCetFiuVYL-cG9eJzF0L87JMHOv7pEnXY9N-EbkLdDTbutbivbufquYJ8RQSnBPY4=w1313-h739-s-no-gm?authuser=3" alt="Cartões plexo"/>
 
-                    <img src={cartoes2} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczOjXBhyvVCoODW0hCnmM-HC5AGbbcDpPl3ouQ73lXnzkVTJnEW-IZ2XmeIDS59hye5UFiO8GlQ8GuZFC_CrxFxU6HEomBZGSy0Gmvezxl8A5bQp9SK17WFx_YO8g4vmxsQ4rVbPAzQhBIhkHJvcrJw=w1313-h739-s-no-gm?authuser=3" alt="C"/>
 
-                    <img src={cartoes3} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczNFdEG5lLuC0jmA0Zun55SHYqxktA6X0BfimjLAxUwNus_IuY0prT5Dw0-GvsrrQ4yq3YP0Z1YLs17hwGYy5Mritv8hxjAxHtKVQUORTeZwlie2ik5aHREZ4l2GSbQ4ZDnoSTG6vwIAWImeAHO_TjY=w1313-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave"/>
 
-                    <img src={cartoes4} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczOwm0C2rw84dmUEuEFPquYWWIkVvQP_9iO4zEJGZ5p9jrzhoD4sKlkNtPI4aO_R5Qo45sU-T647qFObQgM1L2DY8mXJFO7k2bixiV0RxQ8lAKMPdqXzFBPjT0g48ysbdCz_tuzK43tXd66DB355j0Q=w1312-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave"/>
 
-                    <img src={app} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczPMAt1aw8aT3Wu7zK5R38JteeZqf8Dc_ln5yoXSj2PLOoAvSvc0B9N5hJNX08RUh93KHq-TZ07ie5mE1fetrqWv4EY6vnI_0Wp9zIa6_VomnVjDwuJHcDs3RHYIDkBeoj9vXjufjONZI1ZbBldIXVQ=w1312-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave"/>
 
-                    <img src={app2} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczNF_nJAbZ8fYG73NV9q3liaky0_yOHrRMjiZUp7gNPGlbBwS8FN97_l_3p6zK1rAXeJYFOBrGqZT_Owy__ciBXGyGWez9KfGw14L5Wdq8XjZHo2UE4AfYemGZve1h5bQ-8U-sd9zGBdyLiNA_T4Nks=w1313-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave"/>
 
-                    <img src={app3} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczPXZNRdnWxgyvp9Y6HeSGTbI8yhQlM6iqJvJpdc9BLYei60HpiloZ_TvdWE3W51lRVPo8LkoAIdUaOS-dbTO9hPojB150CfFupFT-OEFfU1QJM82vPcWyO7irUJatVXY407XlAW02fvFWNyQn3KNvg=w1313-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave"/>
 
-                    <img src={app4} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczPYIysaNhWB20ZYhqUJSiKO9NQVomBx9aTBXpuG_J02IlGVNc7llUoHY0iq0X6tCo2TPsw9tZ8sZ6eCk5Ik87UCeWfEMLBie_cVMbmSTmsBj5mSgOXmHkRWsB-L5OeMOCVPtRqu9ppXj0cfDfITUvk=w1313-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave"/>
 
-                    <img src={app5} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczMPxPcAmvuMnWlREuuYwhqkFVAcpHodajnpDnfpDy9j9gFB5L0p8C-bf4ljd46KfXFo-O0OcAwG9j3o5wyvy-3BZG6yTEdR3Lr2AJfPftknPBqFJI7Jq_sFuf3CdSKnNIektvgX22eNc_hZfmMUNoY=w1313-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave"/>
 
-                    <img src={site} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczNuAkaek-NHfnxDMwsiTyYp6MWEnrOlcMywS9AzW6wZ999kVHliTOnqZoBjvpUlRXjAJ65wkynEeQLsJqfYtm3yA4PzGKjOmTot9-iAg4INI5gjSzOUfJel1ejcl1TkmTcZG6Tu8ijnMx02ASjTb2g=w1313-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave"/>
 
-                    <img src={placa} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczMCaySEy8pIt-3VLT3fWBL8IsgMJwI72iVbL0BeVzP8KsP6DFKo4hb4B1scURRs3zemMVZNqLGLLbnf8uceEXrhMS9avWN68hinYYsm9FRIqq9Gs95d2pL3jIFb_zNxHi2BY3DB_kq9umE47SHdCmg=w1313-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave"/>
 
-                    <img src={placa2} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczPcwwSQ9CBoCERifK7g1VS7tGuW_MQB7jaV_AzqAn1bj0XGnuZbTYes5ysY6kCjth7RSwDtXEL2QJaxY2naVhLvh58p3C-mG9aWrq3HuL2ggy3WvzFZkyhFsuHEOMiEenZCWWJ0wJ7_dJm3Ac_yWJk=w1312-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave"/>
 
-                    <img src={placa3} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczMrSn6wgB-4iQGv7vL3_2h9euxgSt2RPqDuDgp5kJ9vv8T4Q47qeiL2oulJJtza9rBR4oAg3-H54Jsqp83ChEyswa6OvogdqbuDOk8FrWsmBpyN3azcGPLgOk5oLl_IhiBwKay-BIbzpvbjZ1_7nkM=w1312-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave"/>
 
-                    <img src={placa4} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczO_dUHVajJ4h6lQRfzpJxOvIx4qwdWkT0VS9M4k-b08yItSc2E9JuOhs8ewhXCbHYfsodWbgseGU6NulBDeThw4BYijv47crLiLY383cOKL2sUOdzREN5Z1R2ObtubxbJUihNtYiEXTXv5HhRsbfN0=w1313-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave"/>
 
-                    <img src={carrossel} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczPSO_teWNhpLUEP1nVFVQUorPut42k7zR6uBWAw3_MJwxW4MCixqV9o1wj3HVleMM7FizBmRKP8IitKqgF0mTQh4uwKs6DEKgJWdypL5e2s8KAAvSO61ijOnBQHrIOsUaIQgQoCo8Q4CpUtOYpvyb0=w1313-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave"/>
 
-                    <img src={carrossel2} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczNTX_Zj_jEKuxY8yXQMSMpxNU2c8TShibHsU4vTA2A4DTiv0CB_kwu8bUxn_ISvjlJ8ablJmDyS3Uw4gZIfptjseeb_uH_Xdc5Do18B7pFW00gaL8cU8Il80b9bFrBig-6Iiz97HPzHsVKglTvph9E=w1312-h739-s-no-gm?authuser=3" alt="Capa do projeto Clave"/>
 
-                    <img src={obrigado} alt="Capa do projeto Clave"/>
+                    <img src="https://lh3.googleusercontent.com/pw/AP1GczNg7jt1udh43J0SeKVLckV7_ARhEekaobv_QFMSZOqmYW46QiMvzBlFb2iq9fyFn2RCIQ_9VHP6R2Uh31hk0kQ3IYa_Kay6U92mgHQosE-wl4paz4HDFIZ53K7dwaGHskElZ3Zr6wE1tRcMYuAkWzk=w1440-h406-s-no-gm?authuser=3" alt="Capa do projeto Clave"/>
 
                 </div>
 
