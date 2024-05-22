1d55b7c0dbc39ee422804a12e763f30f16a4f534
felipeCast <castfelipe10@gmail.com>
2024-05-22 17:27:08 -0300

    finalizando layout orbis

--- src/home/pages/orbis/index.tsx
+++ src/home/pages/orbis/index.tsx
@@ -6,7 +6,7 @@ import logo from '../../../assets/logo - header.svg'
 
 export function Orbis() {
     return (
-        <section className='plexo'>
+        <section className='orbis'>
 
             <Link to="/">
                 <img src={logo} alt="Logo de Alex" className='logo-alex' />
@@ -17,18 +17,54 @@ export function Orbis() {
                 <img src="https://lh3.googleusercontent.com/pw/AP1GczPfMnCUvCe1q85VWAPLnyf6Ro44LxeSOCqCaKUtjZO6kQ_fYuFdFt-e1u7BHPEZVIjq0Tyhk1GpReMBLIJa3dFRH--d6YN2RSOFC9zSoOQCR7GC3PQS7qY5RDBPfq52WNA6VLcMbM73VfD0fy65-UGS=w1314-h739-s-no-gm?authuser=3" alt="Capa do projeto Orbis" />
 
                 <div className="orbis-container-text">
-                    <p>
-                        ENG: Plexo is a 100% digital bank, its objective is to offer the user a simplified and secure <br /> banking experience. Your target audience is younger people.
-                        <br /><br />
-                        PT: A Plexo é um banco 100% digital, o seu objetivo é oferecer ao usuário uma <br /> experiência bancária simplificada e segura. Seu púbilico-alvo são pessoas mais jovens.
-                    </p>
+
+                    <div className="text-orbis">
+                        <h3>Clínica Orbis Eyecare</h3>
+                        <h5>PT:</h5>
+                        <p>
+                            A Orbis Eyecare é uma clínica de oftalmologia localizada em Brasília, DF. Este redesign <br /> foi projetado pensando em transmitir ao clientes da marca aquilo que ela se propõe a ser, <br /> uma clínica moderna que atende pessoas de qualquer faixa etária com alto poder aquisitivo.
+                        </p>
+                        <h5>ENG:</h5>
+                        <p>Orbis Eyecare is an ophthalmology clinic located in Brasília DF. This redesign was <br /> designed with the intention of transmitting to the brand's customers what it aims to be, <br />
+                            a modern clinic that serves people of any age group with high purchasing power.</p>
+                    </div>
+
                 </div>
 
-                <img src="https://lh3.googleusercontent.com/pw/AP1GczOrQCUgDhdrwWT3Z0zw5xz90jc8bpzJAi5iaMIZoLh7Iy9KygMvQz9f8xHI-65h-S5brUASvnlE4Q3Tpd3U21-Yu2PcM3LQhO4C3OTjaBWo-NOSfO3_XahoLpa8vmX-uIr8zPcNETBJ0kGoOSJXL-o=w1312-h739-s-no-gm?authuser=3"
-                alt="Paleta de cor projeto plexo"/>
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczN-Uqn9r8sxWJZMDWXneX-dTnZhCI8i0_TN4IHVbFPnxUJRL3WGw2rUxwyQzQCY8XCdggqfYZT3gZcjYlApRrB0cWzxSm86PBVHeJBFhQOxxx2jtoiz1EZ8Aq8SH_jGQb_ExdgCVnSoq4Yg92C1rthP=w1312-h739-s-no-gm?authuser=3" alt="Paleta de cor Orbis" />
 
 
-                <img src="https://lh3.googleusercontent.com/pw/AP1GczOpehC-0mQCj8nuxFj907fKf9Wv3T1guZXN7la8Z-d7RgP7Pn_rMd_BO7mbyxzROebhosHmo7sYpbBrOh1tPw_M6j4RsFIco4_hnnPCBuuqn0SbZFSV4bZ7pInoKcVkEzFeB6est2d5G-1mY_l0ArI=w1313-h739-s-no-gm?authuser=3" alt="Tipografia"/>
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczMXQFbVaH5Izu7F2rOlrnpgcdvRU76mvlDWexlS9iZ6qu5l8GYGXqOObHDXTlFYquZPJs7L5YHPh31Idef8BJALba9n7auwDuI3QYGIHmmPVUpW_ZlGuGRLgkp2ORLbjmPK4EXiW-fB56Bl4qLI2tyn=w1314-h739-s-no-gm?authuser=3" alt="Logo Orbis 1" />
+
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczODKwIa6tBEsaMivzgtg5k_ppjnKESx2iIoOPiDq4aE4a9Fzg4yNooqkQGumJOfEkcxdnQT417EIUewRe3KpbGSgGZFcx00VROCeQqMZmEN7c04dub3taGIrYP0VQxMzd995rgHWV-F3Y4FUJMXBZ22=w1312-h739-s-no-gm?authuser=3" alt="Logo Orbis 2" />
+                
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczM6N63FOlPAFdvaCTs8a5_u-HXiLKfJdKMiT5nMJ-MrSyCc6r3gnLNRcrcYBgTee2MRYLOCnfTWtLZy9HWdRzopr9NS_fSC4zdajqUEJy2GxNzZyLfYq7CnZRDBYYy29U_g2H9oLGTNiR4PFzCQI_6N=w1314-h739-s-no-gm?authuser=3" alt="Logo Orbis reduzida 1" />
+
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczP1Y2naFWuPV-P0UzoMj2V5COdhYcXOQ0vwGcky-FACuqi_OWswoAqs2z5QO8Plnbh7RIJc6tmruWiFvAl31urC64GsI5jpDRORNttHQWCrP0s97UdK3H2ukmCD0zY2pGFrxBW1bn2qNWV6TFyXul8i=w1313-h739-s-no-gm?authuser=3" alt="Logo Orbis reduzida 2" />
+
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczPa8avDhGBvhuOASfQHm_JE8rA74KoUtM0CSx-MUVJSlgCx8NU8R6Si9cegw-karbq-Nj9Tec8P5cKAHIH5Nt1GRDPLQy70Nobitu-_GkpgqoOGkjcF9-mURTvAP7fuw0uERUp874GTERYBT9xi4Xy4=w1312-h739-s-no-gm?authuser=3" alt="Tipografia Orbis" />
+
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczPn0gQixbD1DIAyJgatGveiRaNBZiEc96FLk24OB4WN_g0YW2cgSIykbdx2AZahMG1QTmJ8ke_Jhdos25Rs3mRpZQD-wBuzxcbCotJXM1CQu5k9o6kqjKWS3zJwQqnRlk-gtJpcxLvYjTyRRZZtC8Fz=w1313-h739-s-no-gm?authuser=3" alt="Pattern Orbis" />
+
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczOJOm3fuQT6R2NjNjOPolOErCSxdlD3iK9eNXy5OBKKElAhWXpzgn4h2BOF_94Dyzze1vYJl9E3OuhCgLGEG2PKv8PF524sD74b_42RnDkpVjR39RUSDD4uS6uRWeGTP_mzBEt4zVfxzptp1dZTpZbd=w1312-h739-s-no-gm?authuser=3" alt="Sacola personalizada Orbis" />
+
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczMrDWQh9ku0IDdTdZhVzlxfcmZ55RwhyH5LmRbNaMvnK-_O_I8gt-yY7mVCaTnWyEq1C_7v0hoZgxismiZeiaKktUWRmOBKPxUDEzK-v3FtcHWNfBt-Fx2doU35MRSAdVUrncKm55nl32oO2rcPkuhH=w1312-h739-s-no-gm?authuser=3" alt="Camisa personalizada Orbis" />
+
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczMrpx-cCSRFHvrgiLY7y5fnDcjyFFU0wEL5TivA-bgxDf57T1_hi3NM5DWXr4U82G1z0HAcbAYDrrkIx3PzWNFO8nG_VqMYGrTwfOGIskPmpiMwggP3rU--AyinlQkulzti1Oy1AF1QEwMj7iQiCng4=w1312-h739-s-no-gm?authuser=3" alt="Placa de escritório personalizada Orbis" />
+
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczOAJNp-WjD-woJwXk99-oyBY7IuK8Th3b63WWSEl2wNqBRZHGSaeH0edkX3bhMhhrYek7iTn3AmhW1ls0uXI_B408jdjAJiUL7TufOsO3WEHzZxevRBWI7iJ8AQAnHEHNH8lqLDT9YDLsl6E2merroy=w1313-h739-s-no-gm?authuser=3" alt="Placa de porta personalizada Orbis" />
+
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczMFHFTrvrRVENQLnEKI69ofCM5xibPPBiRDddJ7bpWS9vdgd5I2G3Mr4uyGtBWSQFCVwL98UZMEo3cGDcMWCsvQuIL6IwdS_Igwy7kMkQQ3dMsZ3tO8eUypkarsGTjb0UL7mPh7-m9ca52ie_JICjyR=w1313-h739-s-no-gm?authuser=3" alt="Placa personalizada Orbis" />
+
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczMv9A2t18Pm6VqMOSANRMXZJjZVVJEe-2bsjMi-oa5J4pNcAqSSH38H9biJzahgKLA7gHh1-J61F69V_EYH4RsDjibWCnTToGqEAbY8KDfvRzU3WltB1B0PKNtVbDVQEqTcONqTTn7Hko01WAo81haB=w1313-h739-s-no-gm?authuser=3" alt="Posts redes sociais" />
+
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczMBSLE_shWmLEn6R8jOgppYhUnyh_U1muX9MeqVHZBGrZhVid3Usy5mwZshXK8_G7bPMG0yWErGkrw1RipyqYXVDROTz63BHe5qYH2rH_irGwKGdSXJM9neO7VR3bNd9Y1-sSWPktx02J7OBCl8JTit=w1314-h739-s-no-gm?authuser=3" alt="Aplicações redes sociais" />
+
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczMj3RFvJ3GXOOkpjCu04yJ_U4UxkuY27vX2BuJFa3pryLC2k9EpVpzpN_oMp_OMEDnW2-cmJ0-dtc6aLGHiaLs8XrD1l1b7BKDdchj8XhtWabUDWcMsiH_V-vZ5F_wP2vgTKbRcUGtgzEdlhlKC2HZH=w1108-h739-s-no-gm?authuser=3" alt="Website Orbis" />
+
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczNvjrl4izfp_Pm-bKJQJjwtNffP_iY4CH4cWtA3BvNY0DxDD86QixCbZnSHqygtMHBHIGotozDXNzCXbzy1QvJJfXXPvqRQXWzjXlIV72fnSKTN2QAjQdX8qyS6rJadzSifOD8SZ7KSrBcTsZe4xRxu=w1314-h739-s-no-gm?authuser=3" alt="Comparativo logo Orbis" />
+
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczN7QDgaVbczf7RnH4kwW27yPah17e5xwCedp1IAbU9e60VKZa-l_Gzi-UqGkiAeG5ZIGpH2c_BTK68-KEQy-4a81tEVOmyjhfQ0JofHH9piOJMeMcmQ8zRBYKDhbbUEh6P5bEC0tF4niSth39CJjhBe=w1440-h405-s-no-gm?authuser=3" alt="Agradecimento Orbis" />
 
             </div>
 
